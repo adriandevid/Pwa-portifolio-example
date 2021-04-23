1e76529e9a0bd2cacb0277f4618f9004ecb387b9
adrian devid <adriandevid36@gmail.com>
2021-04-22 21:04:47 -0300

    Pwa example application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdrianP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+        $('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+	};
+	fullHeight();
+
+
+
+	$('#sidebar').toggleClass('active1');
+	$('#sidebarCollapse').css('margin-left', '1em');
+	$('#navbarbutton').on('click', function () {
+		$('#navigatorsbar').toggleClass('actionbar');
+	});
+    $('body').css('zoom', 0.6)
+
+})(jQuery);
